f4a1e7cf52ac24b24843742f711c775a8c1ea23a
Ramon <ramonestii@hotmail.com>
2016-04-06 16:23:37 -0300

    Atualização Final_MAIN

--- src/quites/Main.java
+++ src/quites/Main.java
@@ -15,25 +15,19 @@ public class Main {
     public static void main(String[] args) {
       
       ArrayList <Cliente> clientes = new ArrayList<Cliente>();
-      Cliente cliente1 = new Cliente("Roberval", 00341, "Gol", "2014", "azul");
-      Cliente cliente2 = new Cliente("Ferreira", 00211, "Uno", "2011", "prata");
-      Cliente cliente3 = new Cliente("Silva", 00111, "Vectra", "2009", "preto");
-      Cliente cliente4 = new Cliente("Renan Filip", 00011, "Camaro", "2015", "amarelo");
-      Cliente cliente5 = new Cliente("Damasceno", 00001, "Marcopolo", "2002", "azul");
-      Cliente cliente6 = new Cliente("Arraes", 00200, "Del Rey", "1975", "vermelho");
-      Cliente cliente7 = new Cliente("Gonzalo", 00201, "Brasilia", "1977", "amarelo");
-      Cliente cliente8 = new Cliente("Monica", 00403, "Gol", "2014", "prata");
-      Cliente cliente9 = new Cliente("Eduardo", 00561, "c500", "2014", "vermelho");
+      
+      Cliente cliente1 = new Cliente("Ferreira", 00211, "Uno", "2011", "prata");
+      Cliente cliente2 = new Cliente("Renan Filip", 00011, "Camaro", "2015", "amarelo");
+      Cliente cliente3 = new Cliente("Damasceno", 00001, "Marcopolo", "2002", "azul");
+      Cliente cliente4 = new Cliente("Arraes", 00200, "Del Rey", "1975", "vermelho");
+      
       
       clientes.add(cliente1);
       clientes.add(cliente2);
       clientes.add(cliente3);
       clientes.add(cliente4);
-      clientes.add(cliente5);
-      clientes.add(cliente6);
-      clientes.add(cliente7);
-      clientes.add(cliente8);
-      clientes.add(cliente9);
+     
+      
       
       for (int i = 0; i < clientes.size(); i++) {
         Atendimento atendimento = Atendimento.getInstance();     
@@ -42,6 +36,7 @@ public class Main {
         atendimento.menu(clientes.get(i).resposta);     
         atendimento.infoCarro();      
         JOptionPane.showMessageDialog(null, "Obrigado pela preferência!!!");
+          System.out.println("\n");
         }
       
      
